c22877b6c0f6c1f07e8a68c791a58537dc46e632
Oskar Mikolajczak <o.mikolajczak@getprintbox.com>
2023-05-15 08:14:55 +0200

    finishing touches

--- inits/inits.go
+++ inits/inits.go
@@ -10,7 +10,7 @@ var Session *gocql.Session
 
 func InitDB() {
 	var err error
-	cluster := gocql.NewCluster("185.180.205.124")
+	cluster := gocql.NewCluster("185.180.205.124") // normally, this should be set by environ
 	cluster.Keyspace = "schools"
 	Session, err = cluster.CreateSession()
 	if err != nil {
